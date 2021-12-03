c393adaaa85e388cb3307cef4d57c768071b5257
michael-bailey <mickyb18a@gmail.com>
2021-12-03 19:56:34 +0000

    Reduced day 1 code sample

--- Day_1/main.rs
+++ Day_1/main.rs
@@ -1,34 +1,10 @@
 use std::{fs::{File},error::Error,io::{BufRead,BufReader}};
 
 fn main() -> Result<(), Box<dyn Error>> {
-	let number_vec = BufReader::new(File::open("./Day_1/input.txt")?)
-		.lines()
-		.map(|i| i.unwrap())
-		.map(|i| i.parse::<usize>().expect("not a number"))
-		.collect();
-	println!("part1 result: {}", part1(&number_vec));
-	println!("part2 result: {}", part2(&number_vec));
+	let numbers: Vec<isize> = BufReader::new(File::open("./Day_1/input.txt")?).lines()
+		.map(|i| i.unwrap().parse::<isize>().unwrap()).collect();
+	println!("part1 result: {}", numbers.windows(2).map(|i| if i[0] < i[1] {1} else {0} ).sum::<isize>());
+	println!("part2 result: {}", numbers.windows(3).map(|i| i.iter().sum()).collect::<Vec<isize>>()
+		.windows(2).map(|i| if i[0] < i[1] {1} else {0} ).sum::<isize>());
 	Ok(())
-}
-
-fn part1(numbers: &Vec<usize>) -> isize {
-	let mut result: isize = -1;
-	let mut previous = 0;
-	for number in numbers {
-		if *number > previous {result += 1;} 
-		previous = *number;
-	}
-	result
-}
-
-fn part2(numbers: &Vec<usize>) -> isize {
-	let mut result = -1;
-	let mut previous = 0;
-	for number in numbers.windows(3)
-		.map(|i| i.iter().fold(0, |sum, i| sum+i)) 
-	{
-		if number > previous {result += 1;} 
-		previous = number;
-	}
-	result
 }
